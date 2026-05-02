163fc7ee9a5aa6486236f46eb259786da8ed8e1b
Isis <isis.lopez9112@alumnos.udg.mx>
2026-05-02 15:51:13 +0300

    Proyecto completo

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const pool = require('./db');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json(result.rows);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Error DB');
+    }
+});
+
+app.get('/usuarios', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM usuarios');
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error DB');
+    }
+});
+
+app.post('/usuarios', async (req, res) => {
+    const { nombre, email } = req.body;
+
+    const result = await pool.query(
+        'INSERT INTO usuarios (nombre, email) VALUES ($1, $2) RETURNING *',
+        [nombre, email]
+    );
+
+    res.json(result.rows[0]);
+});
+
+// DELETE
+app.delete('/usuarios/:id', async (req, res) => {
+    const { id } = req.params;
+
+    await pool.query('DELETE FROM usuarios WHERE id = $1', [id]);
+
+    res.send('ok');
+});
+
+// PUT
+app.put('/usuarios/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nombre, email } = req.body;
+
+    const result = await pool.query(
+        'UPDATE usuarios SET nombre = $1, email = $2 WHERE id = $3 RETURNING *',
+        [nombre, email, id]
+    );
+
+    res.json(result.rows[0]);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
